c9c46d11b831a39df17080ce5049ac060bf782a4
Pierrick Vittet <pierrick.vittet.13@gmail.com>
2021-09-06 14:59:30 -0400

    Test django + vue.js

--- djackets/settings.py
+++ djackets/settings.py
@@ -129,9 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, '/djackets_vue/src/assets')
+STATIC_URL = 'djackets_vue/src/assets/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'djackets_vue/src/assets')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'djackets_vue/src/static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
