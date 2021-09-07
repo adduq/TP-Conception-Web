f5d747b7157ae6f2469717931b7fb1083e6104d2
Pierrick Vittet <pierrick.vittet.13@gmail.com>
2021-09-07 02:55:18 -0400

    Test docker

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,13 +1,3 @@
-"""
-Django settings for test project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/dev/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/dev/ref/settings/
-"""
-
 import environ
 from datetime import timedelta
 
