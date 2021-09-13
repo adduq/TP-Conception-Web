a1c221505ba658633c7c588e7f962239f711f7d9
Daisy Turcotte <daisy.turcotte@live.ca>
2021-09-13 12:54:17 -0400

    Supppression des refs à product dans le back end + suppression du storage dans le front-end car référence à product.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -11,25 +11,7 @@ export default createStore({
   },
   mutations: {
     initializeStore(state) {
-      if (localStorage.getItem("cart")) {
-        state.cart = JSON.parse(localStorage.getItem("cart"));
-      } else {
-        localStorage.setItem("cart", JSON.stringify(state.cart));
-      }
-    },
-    addToCart(state, item) {
-      const exists = state.cart.items.filter(
-        (i) => i.product.id === item.product.id
-      );
-
-      if (exists.length) {
-        exists[0].quantite =
-          parseInt(exists[0].quantite) + parseInt(item.quantite);
-      } else {
-        state.cart.items.push(item);
-      }
-
-      localStorage.setItem("cart", JSON.stringify(state.cart));
+     
     },
     setIsLoading(state, status) {
       state.isLoading = status;
@@ -44,8 +26,6 @@ export default createStore({
     },
     clearCart(state) {
       state.cart = { items: [] };
-
-      localStorage.setItem("cart", JSON.stringify(state.cart));
     },
   },
   actions: {},
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -23,7 +23,7 @@ export default {
 	name: "Home",
 	data() {
 		return {
-			latestProducts: [],
+			
 		};
 	},
 	components: {
