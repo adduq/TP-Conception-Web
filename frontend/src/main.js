b43588da45787c9cdc57b7bfd2d1c614cb94baad
Pierrick Vittet <pierrick.vittet.13@gmail.com>
2021-09-26 16:59:04 -0400

    Docker - test v.3.0

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -25,7 +25,7 @@ import store from "./store";
 import axios from "axios";
 import "./assets/reset.css";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000/";
+// axios.defaults.baseURL = "http://127.0.0.1:8000/";
 axios.defaults.xsrfCookieName = "csrftoken";
 axios.defaults.xsrfHeaderName = "X-CSRFToken";
 
