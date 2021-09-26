227770b14cf6553cd488e1a0791766359075c10a
Pierrick Vittet <pierrick.vittet.13@gmail.com>
2021-09-26 11:55:29 -0400

    Docker - test v.2.9

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -65,8 +65,6 @@ INSTALLED_APPS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://127.0.0.1:8080",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "https://communoservice.herokuapp.com",
