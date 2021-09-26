2fcfeef4d74354b706272c2122b11fb7935c791d
Pierrick Vittet <pierrick.vittet.13@gmail.com>
2021-09-26 18:28:51 -0400

    Docker - test v.3.0

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -30,4 +30,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
