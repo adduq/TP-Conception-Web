bdd316cf00c43dc2db9ecba7abbbbc85414054be
Pierrick Vittet <pierrick.vittet.13@gmail.com>
2021-09-26 13:51:24 -0400

    Docker - test v.3.0

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -64,13 +64,14 @@ INSTALLED_APPS = [
     'djoser'
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 
-CORS_ORIGIN_WHITELIST = [
+CORS_ORIGIN_WHITELIST = (
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "https://communoservice.herokuapp.com",
-]
+)
+
 
 # CORS_ALLOWED_ORIGINS = [
 #     "http://localhost:8000",
--- backend/scripts/entrypoint.sh
+++ backend/scripts/entrypoint.sh
@@ -33,6 +33,4 @@
 python3 backend/manage.py makemigrations --no-input
 python3 backend/manage.py migrate --no-input
 
-python3 backend/manage.py runserver 0.0.0.0:$PORT &
-
-npm --prefix ./frontend/ run serve &
+python3 backend/manage.py runserver 0.0.0.0:$PORT
