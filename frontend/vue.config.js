388dc762cc121cbaa2e80ddfea6c19aba8f844ce
Pierrick Vittet <pierrick.vittet.13@gmail.com>
2021-09-26 12:58:05 -0400

    Docker - test v.2.9

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -7,8 +7,6 @@ module.exports = {
 		disableHostCheck: true,
 		historyApiFallback: true,
 		public: "0.0.0.0:8000",
-		// proxy: "https://communoservice.herokuapp.com/",
-		// port: 8000,
 		headers: {
 			"Access-Control-Allow-Origin": "*",
 			"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
