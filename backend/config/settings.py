39662660fe8f8eeb172ebbb15a84514d292b9748
Pierrick Vittet <pierrick.vittet.13@gmail.com>
2021-09-26 12:34:31 -0400

    Docker - test v.2.9

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -64,11 +64,19 @@ INSTALLED_APPS = [
     'djoser'
 ]
 
-CORS_ALLOWED_ORIGINS = [
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "https://communoservice.herokuapp.com",
 ]
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8000",
+#     "http://127.0.0.1:8000",
+#     "https://communoservice.herokuapp.com",
+# ]
 
 # MIDDLEWARE CONFIGURATION
 # ------------------------------------------------------------------------------
