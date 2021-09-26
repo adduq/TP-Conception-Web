7df90ce6473f2fad02c65569f6c41c6bbdf07475
Pierrick Vittet <pierrick.vittet.13@gmail.com>
2021-09-26 02:17:54 -0400

    Docker - test v.2.2

--- backend/scripts/entrypoint.sh
+++ backend/scripts/entrypoint.sh
@@ -33,4 +33,6 @@
 python3 backend/manage.py makemigrations --no-input
 python3 backend/manage.py migrate --no-input
 
-python3 backend/manage.py runserver 0.0.0.0:$PORT
+python3 backend/manage.py runserver 0.0.0.0:$PORT &
+
+npm --prefix ./frontend/ run serve &
