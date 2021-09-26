2bbdf950122cf91c10184f810b3e2dd06eb59e83
Pierrick Vittet <pierrick.vittet.13@gmail.com>
2021-09-26 15:23:07 -0400

    Docker - test v.3.0

--- backend/scripts/entrypoint.sh
+++ backend/scripts/entrypoint.sh
@@ -33,4 +33,5 @@
 python3 backend/manage.py makemigrations --no-input
 python3 backend/manage.py migrate --no-input
 
-python3 backend/manage.py runserver 0.0.0.0:$PORT
+# python3 backend/manage.py runserver 0.0.0.0:$PORT
+gunicorn backend/manage.py -w 4 -b 0.0.0.0:$PORT --daemon --chdir=/app --log-file -
