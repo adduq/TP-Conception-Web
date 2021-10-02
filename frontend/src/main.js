fbafb6337cc05a2848ab4e84aed878d583115d16
Pierrick Vittet <pierrick.vittet.13@gmail.com>
2021-10-02 19:55:24 -0400

    test config local + online

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,8 +5,7 @@ import store from "./store";
 import axios from "axios";
 import "./assets/reset.css";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000/";
-// axios.defaults.baseURL = "https://api-communoservice.herokuapp.com/";
+axios.defaults.baseURL = process.env.VUE_APP_AXIOS_URL;
 axios.defaults.xsrfCookieName = "csrftoken";
 axios.defaults.xsrfHeaderName = "X-CSRFToken";
 
