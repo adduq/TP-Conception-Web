e72b0e328066d7744c513149d99f738d7e459355
Pierrick Vittet <pierrick.vittet.13@gmail.com>
2021-10-03 02:22:05 -0400

    config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,13 +5,9 @@ import store from "./store";
 import axios from "axios";
 import "./assets/reset.css";
 
-console.log(process.env.VUE_APP_AXIOS_URL);
-
 require("dotenv").config();
 
 axios.defaults.baseURL = process.env.VUE_APP_AXIOS_URL;
-
-console.log(process.env.VUE_APP_AXIOS_URL);
 
 axios.defaults.xsrfCookieName = "csrftoken";
 axios.defaults.xsrfHeaderName = "X-CSRFToken";
