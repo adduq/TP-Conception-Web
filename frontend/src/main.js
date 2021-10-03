894c6b049a339fccfa65ec225f202af626928200
Pierrick Vittet <pierrick.vittet.13@gmail.com>
2021-10-03 04:10:25 -0400

    config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,10 @@ import store from "./store";
 import axios from "axios";
 import "./assets/reset.css";
 
-axios.defaults.baseURL = process.env.VUE_APP_AXIOS_URL;
+// if (process.env.VUE_APP_AXIOS_URL !== "http://127.0.0.1:8000/")
+if (process.env.VUE_APP_AXIOS_URL === undefined)
+	axios.defaults.baseURL = "https://api-communoservice.herokuapp.com/";
+
 axios.defaults.xsrfCookieName = "csrftoken";
 axios.defaults.xsrfHeaderName = "X-CSRFToken";
 
